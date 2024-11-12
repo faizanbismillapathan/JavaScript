accc621b77f7dec8b219f67b011c54f93b592972
faizanbismillapathan <derekfk399@gmail.com>
2024-11-12 08:06:15 +0530

    Learned about datatypes

--- variable-and-datatype/variable_datatype.js
+++ variable-and-datatype/variable_datatype.js
@@ -12,5 +12,17 @@ console.log(price);
 Name = "Faizan"
 console.log(Name);
 
+let int = 39;
+console.log(typeof int)
+console.log("Object")
+
+let obj = {
+    name : "Faizan Khan",
+    age : 20,
+    address : "Vadodra"
+};
+// console.log(obj.address);
+console.log(obj['address']);
+
 
 
